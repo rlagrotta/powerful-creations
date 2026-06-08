bb2862024a83fcf6f0bdb3e82ec47dcf3a9a90d5
Roque Lagrotta <roquelagrott@gmail.com>
2026-06-08 15:46:53 -0500

    creacion de menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,25 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsOpen(prev => !prev);
+  }
+
   return (
+    <div className="container mx-auto px-4">
+       <div className="logo"><Image src="/images/logo.svg" alt="logo" width={300} height={300} /></div>
+
     <div className="menu_container">
-      <div className="menu_container__menu-title">
-        Menu
-      </div>
-      <div className="menu_container__menu-links">
+      <button onClick={toggleMenu} className={`menu_container__menu-title ${isOpen ? "open" : ""}`}>
+        {`${isOpen ? "x" : "☰"}`} Menu
+      </button>
+
+      <div className={`menu_container__menu-links ${isOpen ? "open" : ""}`}>
         <ul>
           <li>Link 1</li>
           <li>Link 2</li>
@@ -14,6 +27,10 @@ export default function Home() {
           <li>Link 4</li>
         </ul>
       </div>
+    </div>
+
+       
+
     </div>
   );
 }
